d947a1f3d925bf718b8edaabf6940d05ab7b4493
Dennis Czombera <milchschaumschluerfer@gmail.com>
2015-08-23 14:22:25 +0200

    Generate class methods ::heroes and ::items dynamically using define_singleton_method

--- lib/steam_web_api/steam_web_api.rb
+++ lib/steam_web_api/steam_web_api.rb
@@ -5,52 +5,41 @@ module SteamWebApi
       # Singleton
       class << self
 
-        def heroes
-          # used for activerecord-import for bulk inserting data
-          updated_heroes = []
-          heroes_from_api, status = ApiCall::get_heroes
-          # Check if API call was successful
-          if status == 200
-            heroes_from_db = Hero.all.try(:index_by, &:steam_id)
-            
-            heroes_from_api.map do |api_hero|
-              if db_hero = heroes_from_db[api_hero.id]
-                db_hero.update_status(api_hero)
-                db_hero.save if db_hero.changed?
-              else
-                # Let's create a new hero since id wasn't found in db
-                updated_heroes << Hero.new(steam_id: api_hero.id, name: api_hero.name, localized_name: api_hero.localized_name)
+        def define_refresh_method(klass)
+          name = klass.name.downcase.pluralize
+          define_singleton_method(name) do
+            # used for activerecord-import for bulk inserting data
+            updated_records = []
+            data_from_api, status =  ApiCall.send "get_#{name}"
+            # Check if API call was successful
+            if status == 200
+              records_from_db = klass.all.try(:index_by, &:steam_id)
+
+              data_from_api.map do |api_data|
+                if db_record = records_from_db[api_data.id]
+                  db_record.update_status(api_data)
+                  db_record.save if db_record.changed?
+                else
+                  # TODO: needs to be refactored because it's too specific
+                  # Let's create a new record since id wasn't found in db
+                  if klass.name == "Hero"
+                    updated_records << klass.new(steam_id: api_data.id, name: api_data.name, localized_name: api_data.localized_name)
+                  else
+                    updated_records << klass.new(steam_id: api_data.id, name: api_data.name, cost: api_data.cost,
+                                              secret_shop: api_data.secret_shop, side_shop: api_data.side_shop,
+                                              recipe: api_data.recipe)
+                  end
+                end
               end
             end
             # Save records in db using activerecord-import method
-            Hero.import updated_heroes if updated_heroes.any?
-          end
-        end
-
-        def items
-          # used for activerecord-import for bulk inserting data
-          updated_items = []
-          items_from_api, status = ApiCall::get_items
-          # Check if API call was successful
-          if status == 200
-            items_from_db = Item.all.try(:index_by, &:steam_id)
-
-            items_from_api.map do |api_item|
-              if db_item = items_from_db[api_item.id]
-                db_item.update_status(api_item)
-                db_item.save if db_item.changed?
-              else
-                # Let's create a new item since id wasn't found in db
-                updated_items << Item.new(steam_id: api_item.id, name: api_item.name, cost: api_item.cost,
-                                          secret_shop: api_item.secret_shop, side_shop: api_item.side_shop,
-                                          recipe: api_item.recipe)
-              end
-            end
-            # Save records in db using activerecord-import method
-            Item.import updated_items if updated_items.any?
+            klass.import updated_records if updated_records.any?
           end
         end
       end
+
+      define_refresh_method Hero # define "heroes" method
+      define_refresh_method Item # define "items"" method
     end
 
     # Singleton class used for Api calls of the Steam Web Api
